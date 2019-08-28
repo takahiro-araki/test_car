9654920928e3b074c2adc7cee4b63242e661bc05
takahiroaraki <s01110002e1117.gmail.com>
2019-08-28 10:51:39 +0900

    シェアリングＣａｒ課題　開始。

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class Car {
+	
+	private Integer speed;
+	private String bodyColor;
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	public String getBodyColor() {
+		return bodyColor;
+	}
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + "]";
+	}
+	
+
+}
